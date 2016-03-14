9f4eb03d3cbaf573f52efa127618dac2e209b52c
Andrew Duthie <andrew@andrewduthie.com>
2016-03-14 10:00:09 -0400

    CPT: Render PostType on types routes

--- client/my-sites/types/main.jsx
+++ client/my-sites/types/main.jsx
@@ -1,13 +1,22 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 /**
  * Internal dependencies
  */
 import Main from 'components/main';
+import PostType from 'my-sites/post-type';
 
-export default function Types() {
-	return <Main>Custom Post Type Listing</Main>;
+export default function Types( { type } ) {
+	return (
+		<Main>
+			<PostType type={ type } />
+		</Main>
+	);
 }
+
+Types.propTypes = {
+	type: PropTypes.string.isRequired
+};
